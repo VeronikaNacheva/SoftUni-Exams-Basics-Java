a7a7584fd1f4bd4a04423ab406c366769f6efe75
veronikaaa86 <31905701+veronikaaa86@users.noreply.github.com>
2018-03-08 16:18:07 +0200

    Adding New Exam

---
+++ Exam17July2016/P01_Money.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P01_Money {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int bitcoin = Integer.parseInt(scanner.nextLine());
+        double cny = Double.parseDouble(scanner.nextLine());
+        double tax = Double.parseDouble(scanner.nextLine());
+
+        double bitcoinLv = bitcoin * 1168;
+        double cnyUSD = cny * 0.15;
+        double usdToLv = 1.76;
+        double eurToLv = 1.95;
+        double cnyToLv = cnyUSD * usdToLv;
+
+        double allLv = bitcoinLv + cnyToLv;
+        double taxPrecent = (tax / 100) * allLv;
+        double LvMinusTax = allLv - taxPrecent;
+
+        double allEur = LvMinusTax / eurToLv;
+
+        System.out.printf("%.2f", allEur);
+    }
+}
---
+++ Exam17July2016/P06_StopNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P06_StopNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = Integer.parseInt(scanner.nextLine());
+        int M = Integer.parseInt(scanner.nextLine());
+        int S = Integer.parseInt(scanner.nextLine());
+
+        for (int i = M; i >= N; i--) {
+            if ((i % 2 == 0) && (i % 3 == 0)) {
+                if (i == S) {
+                    break;
+                }
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
