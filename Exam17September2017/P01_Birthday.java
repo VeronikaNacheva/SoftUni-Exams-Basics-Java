27055b0e607d3c0ec613d8c6026c3fb685411b80
veronikaaa86 <31905701+veronikaaa86@users.noreply.github.com>
2018-03-08 18:27:41 +0200

    Adding New Exam

---
+++ Exam17September2017/P01_Birthday.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P01_Birthday {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int length = Integer.parseInt(scanner.nextLine());
+        int width = Integer.parseInt(scanner.nextLine());
+        int height = Integer.parseInt(scanner.nextLine());
+        double percent = Double.parseDouble(scanner.nextLine());
+
+        double volumeAqua = (length * width * height) * 0.001;
+        double minusAcc = volumeAqua - (volumeAqua * (percent * 0.01));
+
+        System.out.printf("%.3f", minusAcc);
+    }
+}
