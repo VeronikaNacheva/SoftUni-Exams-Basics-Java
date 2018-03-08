5e0bc13be4e9c682a1b47afb429ae8609eac1c7b
veronikaaa86 <31905701+veronikaaa86@users.noreply.github.com>
2018-03-09 00:50:35 +0200

    Adding ne Exam

---
+++ Exam19March2017Morning/P02_Cups.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class P02_Cups {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int needCups = Integer.parseInt(scanner.nextLine());
+        int countWorkers = Integer.parseInt(scanner.nextLine());
+        int workDays = Integer.parseInt(scanner.nextLine());
+
+        double allHour = countWorkers * (workDays * 8);
+        double haveCups = Math.floor(allHour / 5);
+
+        double need = 0;
+        double left = 0;
+
+        if (haveCups < needCups) {
+            need = needCups - haveCups;
+            System.out.printf("Losses: %.2f", need * 4.2);
+        } else {
+            left = haveCups - needCups;
+            System.out.printf("%.2f extra money", left * 4.2);
+        }
+    }
+}
