7bbf8d9d444366c3988e9ed431ab67b42e644660
veronikaaa86 <31905701+veronikaaa86@users.noreply.github.com>
2018-03-10 07:08:33 +0200

    Adding New Exam

---
+++ Exam5November2017/P06_LuckyNumbers.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class P06_LuckyNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        String happy = "";
+        int sumOne = 0;
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                for (int k = 1; k <= 9; k++) {
+                    for (int l = 1; l <= 9; l++) {
+                        sumOne = i + j;
+                        if (i + j == k + l) {
+                            happy = i + "" + j + "" + k + "" + l + "";
+                            if (n % sumOne == 0) {
+                                System.out.print(happy + " ");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
